cabb494fe884a98875f9aac8c3d17b3ba4518ab4
Riki Saito <rjsaito@gmail.com>
2017-01-03 19:32:25 -0600

    Add files via upload

---
+++ General/workingTempFile.R
@@ -0,0 +1,31 @@
+wtf = function (x) {
+  tempFilePath = paste(tempfile(), ".csv")
+  tempPath = dirname(tempFilePath)
+  preferredFile = paste(deparse(substitute(x)), ".csv", sep = "")
+  preferredFilePath = file.path(tempPath, preferredFile)
+
+  if(length(dim(x))>2){
+    stop('Too many dimensions')
+  }
+  if(is.null(dim(x))){
+    x = as.data.frame(x)
+  }
+  if (is.null(rownames(x))) {
+    tmp = 1:nrow(x)
+  }else {
+    tmp = rownames(x)
+  }
+  rownames(x) = NULL
+  x = data.frame(x)
+  WriteAttempt = try(
+    write.table(x, file=preferredFilePath, quote=TRUE, sep=",", na="",
+                row.names=FALSE, qmethod="double"),
+    silent = TRUE)
+  if ("try-error" %in% class(WriteAttempt)) {
+    write.table(x, file=tempFilePath, , quote=TRUE, sep=",", na="",
+                row.names=FALSE, qmethod="double")
+    shell.exec(tempFilePath)
+  } else {
+    shell.exec(preferredFilePath)
+  }
+}
